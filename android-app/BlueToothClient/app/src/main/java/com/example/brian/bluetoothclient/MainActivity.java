8038c38b19a2317bf5d039c17caf5906a3861534
Brian <BLester125@gmail.com>
2015-08-23 20:02:18 -0400

    Adding inital app

---
+++ android-app/BlueToothClient/app/src/main/java/com/example/brian/bluetoothclient/MainActivity.java
@@ -0,0 +1,182 @@
+package com.example.brian.bluetoothclient;
+
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+    TextView out;
+    private static final int REQUEST_ENABLE_BT = 1;
+    private BluetoothAdapter btAdapter = null;
+    private BluetoothSocket btSocket = null;
+    private OutputStream outStream = null;
+
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    private static String address = "EC:55:F9:DA:2E:0B";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        out = (TextView) findViewById(R.id.out);
+        out.append("\n...In Create()...");
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        CheckBTState();
+    }
+
+    public void onStart() {
+        super.onStart();
+        out.append("\n...In onStart()...");
+    }
+
+    public void onResume() {
+        super.onResume();
+        out.append("\n...In onResume...\n...Attempting client connect...");
+        BluetoothDevice device = btAdapter.getRemoteDevice(address);
+        try {
+            btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
+        } catch (IOException e) {
+            AlertBox("Fatal Error", "In onResume() and socket create failed: " + e.getMessage() + ".");
+        }
+        btAdapter.cancelDiscovery();
+        try {
+            btSocket.connect();
+            out.append("\n...Connection established and data link opened...");
+        } catch (IOException e) {
+            try {
+                btSocket.close();
+            } catch (IOException e2) {
+                AlertBox("Fatal Error", "In onResume() and unable to close Socket" + e.getMessage() + ".");
+            }
+        }
+        out.append("\n...Sending message to server...");
+        String message = "Brain;Mord;PGH;1;Kofi;\n";
+        out.append("\n\n...The message that will be sent is: " + message);
+        try {
+            outStream = btSocket.getOutputStream();
+        } catch (IOException e) {
+            AlertBox("Fatal Error", "Out put stream creation failed " + e.getMessage() + ".");
+        }
+        byte[] msgBuffer = message.getBytes();
+        try {
+            outStream.write(msgBuffer);
+        } catch (IOException e) {
+            String msg = "In onResume() and on exception occured during write: " + e.getMessage() + ".";
+            if (address.equals("00:00:00:00:00:00")){
+                msg = msg + "\n\nUpdate Address";
+            }
+            msg = msg +"\n\nCheck SPP UUID";
+            AlertBox("Fatal Error", msg);
+        }
+    }
+
+    public void onPause() {
+        super.onPause();
+
+        //out.append("\n...Hello\n");
+        InputStream inStream;
+        try {
+            inStream = btSocket.getInputStream();
+            BufferedReader bReader=new BufferedReader(new InputStreamReader(inStream));
+            String lineRead=bReader.readLine();
+            out.append("\n..."+lineRead+"\n");
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        out.append("\n...In onPause()...");
+
+
+
+        if (outStream != null) {
+            try {
+                outStream.flush();
+            } catch (IOException e) {
+                AlertBox("Fatal Error", "In onPause() and failed to flush output stream: " + e.getMessage() + ".");
+            }
+        }
+
+        try     {
+            btSocket.close();
+        } catch (IOException e2) {
+            AlertBox("Fatal Error", "In onPause() and failed to close socket." + e2.getMessage() + ".");
+        }
+    }
+
+    public void onStop() {
+        super.onStop();
+        out.append("\n...In onStop()...");
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        out.append("\n...In onDestroy()...");
+    }
+
+    private void CheckBTState() {
+        // Check for Bluetooth support and then check to make sure it is turned on
+
+        // Emulator doesn't support Bluetooth and will return null
+        if(btAdapter==null) {
+            AlertBox("Fatal Error", "Bluetooth Not supported. Aborting.");
+        } else {
+            if (btAdapter.isEnabled()) {
+                out.append("\n...Bluetooth is enabled...");
+            } else {
+                //Prompt user to turn on Bluetooth
+                Intent enableBtIntent = new Intent(btAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+        }
+    }
+
+    public void AlertBox( String title, String message ){
+        new AlertDialog.Builder(this)
+                .setTitle( title )
+                .setMessage( message + " Press OK to exit." )
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        finish();
+                    }
+                }).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- library/SPPServer.py
+++ library/SPPServer.py
@@ -4,7 +4,6 @@ import threading
 from library.event import *
 
 name="bt_server"
-target_name="siggen"
 uuid="00001101-0000-1000-8000-00805F9B34FB"
 
 def runServer():
